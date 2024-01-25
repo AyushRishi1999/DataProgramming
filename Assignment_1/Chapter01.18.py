a757f231cd017c997ab6af36afd9d0d09c93d837
AyushRishi1999 <shoanfrost@gmail.com>
2024-01-25 15:59:29 -0500

    Chapter 1

---
+++ Assignment_1/Chapter01.18.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.right((180-36)/2)
+turtle.forward(150)
+turtle.right(180-36)
+turtle.forward(150)
+turtle.right(180-36)
+turtle.forward(150)
+turtle.right(180-36)
+turtle.forward(150)
+turtle.right(180-36)
+turtle.forward(150)
+
+turtle.done()
